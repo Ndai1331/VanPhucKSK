fb33a7d4c6f080c3458aa0e6d9b7ca5368d0136c
NDai <longnguyen13th1d.pou@gmail.com>
2025-06-30 11:21:13 +0700

    [WEB] init

--- buid_and_push.sh
+++ buid_and_push.sh
@@ -1,5 +1,5 @@
 # Build and push WebApi image
 echo "Building CoreAdminWeb image for AMD64..."
-docker build --platform linux/amd64 -t longnguyen1331/drcore-admin-qlcl-web:latest -f CoreAdminWeb/Dockerfile .
+docker build --platform linux/amd64 -t longnguyen1331/van-phuc-web:latest -f CoreAdminWeb/Dockerfile .
 echo "Pushing CoreAdminWeb image..."
-docker push longnguyen1331/drcore-admin-qlcl-web:latest
+docker push longnguyen1331/van-phuc-web:latest
