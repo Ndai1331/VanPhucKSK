9d886742ea1dd2abaa6b24cc633c1a034cb2d1df
Mạnh Quốc <quoctm@etc.vn>
2025-07-19 22:43:57 +0700

    update

---
+++ CoreAdminWeb/Enums/TrangThaiHoatDong.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace CoreAdminWeb.Enums
+{
+    public enum TrangThaiHoatDong
+    {
+        [Description("Đang hoạt động")]
+        active,
+        [Description("Ngừng hoạt động")]
+        deactive
+    }
+}
