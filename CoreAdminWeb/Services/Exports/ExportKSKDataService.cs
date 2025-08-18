61d363d3ca6c594fd91c7d05bbf1fdfabccdb467
Mạnh Quốc <quoctm@etc.vn>
2025-08-18 07:50:30 +0700

    update

---
+++ CoreAdminWeb/Services/Exports/ExportKSKDataService.cs
@@ -0,0 +1,74 @@
+﻿using CoreAdminWeb.Hubs;
+using CoreAdminWeb.Model.KhamSucKhoes;
+using CoreAdminWeb.Model.RequestHttps;
+using CoreAdminWeb.Model.Settings;
+using CoreAdminWeb.Services.BaseServices;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CoreAdminWeb.Services.Imports
+{
+    public class ExportKSKDataService
+    {
+        private readonly IHubContext<ImportProgressHub> _hubContext;
+        private readonly IBaseDetailService<SoKhamSucKhoeModel> _soKhamSucKhoeService;
+        private readonly IBaseDetailService<KhamSucKhoeChuyenKhoaModel> _khamSucKhoeChuyenKhoaService;
+        private readonly IBaseDetailService<KhamSucKhoeKetLuanModel> _khamSucKhoeKetLuanService;
+        public ExportKSKDataService(IHubContext<ImportProgressHub> hubContext, IServiceScopeFactory serviceScopeFactory)
+        {
+            _hubContext = hubContext;
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                _soKhamSucKhoeService = scope.ServiceProvider.GetRequiredService<IBaseDetailService<SoKhamSucKhoeModel>>();
+                _khamSucKhoeChuyenKhoaService = scope.ServiceProvider.GetRequiredService<IBaseDetailService<KhamSucKhoeChuyenKhoaModel>>();
+                _khamSucKhoeKetLuanService = scope.ServiceProvider.GetRequiredService<IBaseDetailService<KhamSucKhoeKetLuanModel>>();
+            }
+        }
+
+
+        public async Task ExportFromExaminationWithProgressAsync(string connectionId,
+                                                           List<int> soKhamSucKhoes,
+                                                           SettingModel setting,
+                                                           CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _hubContext.Clients.Client(connectionId)
+                .SendAsync("ExportExamination", $"Đang khởi tạo dữ liệu", false);
+            }
+            catch (Exception ex)
+            {
+                await _hubContext.Clients.Client(connectionId)
+                .SendAsync("ExportExamination", $"Lỗi khi import: {ex.Message}", false);
+            }
+        }
+        static async Task<List<T>> BatchQueryAsync<T>(Func<List<string>, Task<RequestHttpResponse<List<T>>>> queryFunc, List<string> ids, int batchSize = 200)
+        {
+            var results = new List<T>();
+            foreach (var batch in ids.Chunk(batchSize))
+            {
+                var res = await queryFunc(batch.ToList());
+                if (res.IsSuccess && res.Data != null)
+                {
+                    results.AddRange(res.Data);
+                }
+            }
+            return results;
+        }
+
+        static async Task BatchExecuteAsync<T>(List<T> items, Func<List<T>, Task<RequestHttpResponse<List<T>>>> execFunc, int batchSize = 100)
+        {
+            foreach (var batch in items.Chunk(batchSize))
+            {
+                await execFunc(batch.ToList());
+            }
+        }
+
+        static async Task BatchExecuteAsync<T>(List<T> items, Func<List<T>, Task<RequestHttpResponse<bool>>> execFunc, int batchSize = 100)
+        {
+            foreach (var batch in items.Chunk(batchSize))
+            {
+                await execFunc(batch.ToList());
+            }
+        }
+    }
+}
