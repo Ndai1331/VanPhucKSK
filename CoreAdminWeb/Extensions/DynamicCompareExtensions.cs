3d8eac666043816a4264ba53464244a248dd6890
Mạnh Quốc <manhquoc11@gmail.com>
2025-09-22 23:42:16 +0700

    [WEB] update compare helper

--- CoreAdminWeb/Extensions/DynamicCompareExtensions.cs
+++ CoreAdminWeb/Extensions/DynamicCompareExtensions.cs
@@ -42,6 +42,19 @@ namespace CoreAdminWeb.Extensions
                 return true;
             }
 
+            // Nếu là DateTime hoặc DateTime?, chỉ so sánh phần ngày (dd/MM/yyyy)
+            if ((value is DateTime || value is DateTime?) &&
+                (dynamicValue is DateTime || dynamicValue is DateTime?))
+            {
+                var valueDate = value is DateTime dt1 ? dt1 : ((DateTime?)value)?.Date;
+                var dynamicDate = dynamicValue is DateTime dt2 ? dt2 : ((DateTime?)dynamicValue)?.Date;
+
+                string valueDateStr = valueDate?.ToString("dd/MM/yyyy") ?? string.Empty;
+                string dynamicDateStr = dynamicDate?.ToString("dd/MM/yyyy") ?? string.Empty;
+
+                return !valueDateStr.Equals(dynamicDateStr);
+            }
+
             // Nếu value là object (không phải kiểu nguyên thủy hoặc string), so sánh theo trường "Id"
             var valueType = value.GetType();
             if (!valueType.IsPrimitive && valueType != typeof(string))
@@ -68,43 +81,6 @@ namespace CoreAdminWeb.Extensions
             }
 
             return !value.Equals(dynamicValue);
-        }
-
-        /// <summary>
-        /// Trả về class CSS nếu tồn tại property trong dynamicObj.
-        /// </summary>
-        /// <param name="value">Giá trị hiện tại</param>
-        /// <param name="dynamicObj">Đối tượng dynamic để so sánh</param>
-        /// <param name="propertyName">Tên property cần kiểm tra</param>
-        /// <param name="highlightClass">Tên class highlight (mặc định: "border-red-500")</param>
-        /// <returns>Chuỗi class CSS</returns>
-        public static string GetHighlightClassIfChanged(this object? dynamicObj, string propertyName, string highlightClass = "border-red-500")
-        {
-            if (dynamicObj == null || string.IsNullOrEmpty(propertyName))
-            {
-                return string.Empty;
-            }
-
-            // Handle ExpandoObject
-            if (dynamicObj is IDictionary<string, object> expandoDict && expandoDict.ContainsKey(propertyName))
-            {
-                return highlightClass;
-            }
-
-            var dynamicType = dynamicObj.GetType();
-            var prop = dynamicType.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-            if (prop != null)
-            {
-                return highlightClass;
-            }
-
-            var field = dynamicType.GetField(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-            if (field != null)
-            {
-                return highlightClass;
-            }
-
-            return string.Empty;
         }
 
         /// <summary>
@@ -157,6 +133,22 @@ namespace CoreAdminWeb.Extensions
                     return highlightClass;
                 }
 
+                return string.Empty;
+            }
+
+            if ((changedValue is DateTime || changedValue is DateTime?) &&
+                (originalValue is DateTime || originalValue is DateTime?))
+            {
+                var changedDate = changedValue is DateTime dt1 ? dt1 : ((DateTime?)changedValue)?.Date;
+                var originalDate = originalValue is DateTime dt2 ? dt2 : ((DateTime?)originalValue)?.Date;
+
+                string changedDateStr = changedDate?.ToString("dd/MM/yyyy") ?? string.Empty;
+                string originalDateStr = originalDate?.ToString("dd/MM/yyyy") ?? string.Empty;
+
+                if (!changedDateStr.Equals(originalDateStr))
+                {
+                    return highlightClass;
+                }
                 return string.Empty;
             }
 
